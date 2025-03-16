c6f8a813839f3ff02e5989abbf2a62e54bf75b04
p238049y <p238049y@gmail.com>
2025-03-16 23:57:51 +0900

    [add] Userモデルのテストを追加
    #29

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#age' do
+    context '20年前の生年月日の場合' do
+      let(:user) { 
+        User.new(birthday: Time.zone.now - 20.years.ago)
+      }
+
+      it '年齢が20歳であること' do
+        expect(user.age).to eq 20
+      end
+    end
+  end
 end
